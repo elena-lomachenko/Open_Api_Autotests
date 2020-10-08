93aa5a3b39f929de7b2b4dd30afabbd864dcff7d
Elena Lomachenko <lomachenko@bcs.u>
2020-10-08 21:37:53 +0700

    [WIP]

--- src/main/java/Other/OptionsTests.java
+++ src/main/java/Other/OptionsTests.java
@@ -1,4 +1,56 @@
 package Other;
 
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class OptionsTests {
+
+  public static final String CHROME_DRIVER_PATH = "C:\\Users\\elena\\drivers\\chromedriver_win32\\chromedriver.exe";
+  public WebDriver driver;
+  public static WebDriverWait wait;
+
+  // Создание свойств драйвера - задаём путь, отключаем расширения
+  static ChromeOptions getChromeOptions() {
+    System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
+
+    final var options = new ChromeOptions();
+    options.addArguments("--disable-notifications");
+    options.setExperimentalOption("useAutomationExtension", false);
+    options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.IGNORE);
+
+    return options;
+  }
+
+  // Создание сервсиа драйвера - логирование и прочее тут
+  static ChromeDriverService getChromeService() {
+    return new ChromeDriverService.Builder()
+        .withSilent(true)
+        .withLogFile(null)
+        .build();
+  }
+
+  // Создаём сам драйвер
+  public void start() {
+
+    var s = getChromeService();
+    var o = getChromeOptions();
+
+    driver = new ChromeDriver(s, o);
+
+    final var options = new ChromeOptions();
+    options.addArguments("--disable-notifications");
+    options.setExperimentalOption("useAutomationExtension", false);
+    options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.IGNORE);
+
+    wait = new WebDriverWait(driver, 10);
+  }
+
+  public void stop() {
+
+  }
 }
+
--- src/main/java/Tests/FirstTest.java
+++ src/main/java/Tests/FirstTest.java
@@ -1,9 +1,26 @@
-public class FirstTest {
+package Tests;
+
+import Other.OptionsTests;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class FirstTest extends OptionsTests {
+
   public static void main(String[] args) {
     System.out.println("hello world");
     System.err.println("Btoom!");
-
+  }
+    @Test
+    public void myFirstTest() {
+      driver.navigate().to("http://www.google.com");
+      driver.findElement(By.name("q")).sendKeys("webdriver");
+      driver.findElement(By.id("tsf")).click();
+      driver.findElement(By.name("btnK")).click();
+      wait.until(titleIs("webdriver - Поиск в Google"));
+    }
 
   }
 
-}
+
